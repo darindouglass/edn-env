599d95d74216e62569c1e039f50f794bcc637595
Darin Douglass <ddouglass@barracuda.com>
2020-09-17 10:54:07 -0400

    Add clojure as a dep

--- project.clj
+++ project.clj
@@ -1,2 +1,3 @@
 (defproject douglass/edn-env "0.1.0"
-  :profiles {:test {:resource-paths ["test/resources"]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}
+             :test {:resource-paths ["test/resources"]}})
