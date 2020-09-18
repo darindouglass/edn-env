05d533958c32ed4bce371318b0e030315319e057
Darin Douglass <ddouglass@barracuda.com>
2020-09-18 12:48:56 -0400

    Don't error on missing files when loading config

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
-(defproject douglass/edn-env "0.1.0"
+(defproject douglass/edn-env "0.1.1"
+  :description "A small library to overlay environment variables onto a config"
+  :license "EPL 1.0"
+  :url "https://github.com/DarinDouglass/edn-env"
+  :deploy-repositories [["clojars" {:sign-releases false}]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}
              :test {:resource-paths ["test/resources"]}})
